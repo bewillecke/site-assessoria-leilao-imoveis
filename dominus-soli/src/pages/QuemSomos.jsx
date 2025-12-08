f3789c82a60bb35daf0716a6e19bb1da3b0f1e53
Bernardo Willecke Martins da Rocha <bewillecke@gmail.com>
2025-12-08 19:26:00 -0300

    correção na página QuemSomos

--- dominus-soli/src/pages/QuemSomos.jsx
+++ dominus-soli/src/pages/QuemSomos.jsx
@@ -20,6 +20,8 @@
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+
+export default function QuemSomos() {
   return (
     <>
       <Header />
